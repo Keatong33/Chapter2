a8295c723fa2ebf801be2f70079bf24ed3c5055a
Greenwalt <KG155781@davenportschools.org>
2019-04-18 12:24:19 -0500

    update

--- src/QuartstoGalllonsInteractive.java
+++ src/QuartstoGalllonsInteractive.java
@@ -2,15 +2,24 @@ import java.util.Scanner;
 public class QuartstoGalllonsInteractive {
     public static void main(String [] args){
         int numberOfQuartsInputed;
+
+        Scanner keyboard = new Scanner(System.in);
+            System.out.println("Please enter the number of quarts needed to do the job>>>");
+            numberOfQuartsInputed = keyboard.nextInt();
+
+            displayMath(numberOfQuartsInputed);
+
+
+
+    }
+    public static void displayMath(int numberOfQuartsInputed){
         final int NUM_QUARTS_IN_GALLON = 4;
         int remainderOfQuarts;
         int numberOfGallons;
-        Scanner keyboard = new Scanner(System.in);
-        System.out.println("Please enter the number of quarts needed to do the job>>>");
-        numberOfQuartsInputed = keyboard.nextInt();
         numberOfGallons = numberOfQuartsInputed / NUM_QUARTS_IN_GALLON;
         remainderOfQuarts = numberOfQuartsInputed % NUM_QUARTS_IN_GALLON;
         System.out.println("The number of gallons ordered is " + numberOfGallons +
                 " and the number of quarts remaining is " + remainderOfQuarts);
+
     }
 }
